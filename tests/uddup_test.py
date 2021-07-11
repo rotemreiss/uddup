3dbed9c783b36e4ad3da1fd6e7980e7e924b5173
Rotem Reiss <reiss.r@gmail.com>
2021-07-11 12:58:51 +0300

    Add more unit-tests

--- tests/uddup_test.py
+++ tests/uddup_test.py
@@ -5,6 +5,45 @@ import uddup.main as u
 import pytest
 
 
+def test_split_by_base_urls():
+    urls = [
+        'https://www.example.com/foo?foo=1&bar=2',
+        'http://www.example.com/foo',
+        'http://www.example2.com/foo',
+        'http://www.example.com/bar',
+        'https://www.example.com/bar'
+    ]
+
+    expected_result = [
+        {
+            u.get_parsed_url('http://www.example.com/foo'),
+            u.get_parsed_url('http://www.example.com/bar')
+        },
+        {
+            u.get_parsed_url('http://www.example2.com/foo')
+        },
+        {
+            u.get_parsed_url('https://www.example.com/foo?foo=1&bar=2'),
+            u.get_parsed_url('https://www.example.com/bar')
+        }
+    ]
+
+    assert u.split_by_base_urls(urls) == expected_result
+
+
+def test_is_same_domain():
+    url1 = u.get_parsed_url('http://www.example.com/foo')
+    url2 = u.get_parsed_url('http://www.example.com/foo/bar')
+    url3 = u.get_parsed_url('https://www.example.com/foo')
+    url4 = u.get_parsed_url('http://www.example2.com/foo')
+    url5 = u.get_parsed_url('https://www.example.com/bar')
+
+    pytest.assume(u.is_same_domain(url1, url2) is True)
+    pytest.assume(u.is_same_domain(url1, url3) is False)
+    pytest.assume(u.is_same_domain(url1, url4) is False)
+    pytest.assume(u.is_same_domain(url3, url5) is True)
+
+
 def test_get_query_params_keys():
     test_url = u.get_parsed_url("https://www.example.com/?foo=1&bar=2")
     params = u.get_query_params_keys(test_url.query)
